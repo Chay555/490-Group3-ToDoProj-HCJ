6b6fd06d03edce6a760cd56221d17e3251894177
Chay555 <chayceward1@gmail.com>
2022-09-19 12:19:46 -0400

    login page start

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+//Start session
+session_start();
+
+//Initialize login variables
+$username = "test1";
+$password = "test2";
+$username_err = $password_err = $login_err = "";
+
+//Variable test -- REMOVE LATER
+echo $username;
+echo $password;
+
+
+?>
