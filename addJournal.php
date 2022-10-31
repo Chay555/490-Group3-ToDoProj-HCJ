5663bd521d5f2950710b950d5bd409549c5c7566
Vincent Stylo <j.martinez.torres@icloud.com>
2022-10-30 23:11:37 -0400

    Displays Previous Entries!

--- addJournal.php
+++ addJournal.php
@@ -1,16 +1,12 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
-        $conn = mysqli_connect('localhost', 'root', 'YES', 'journal_entry')
-        or die("Connection Failed:" .mysqli_connect_error());
-        if (isset($_POST['journalEntry'])) {
-            $journalEntry = $_POST['journalEntry'];
-            $sql = "INSERT INTO `journals` (`journal_entry`) VALUES ('$journalEntry')";
-            $query = mysqli_query($conn, $sql);
-            if ($query) {
-                echo 'Entry Successful! It worked Professor Foultz!';
-                
-            } else {
-                echo 'Error Ocurred, oh god dont look professor';
-            }
-        }
+include_once 'connect.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit']) && isset($_POST['journalEntry'])) {
+    $journalEntry = $_POST['journalEntry'];
+    $sql = "INSERT INTO `journals` (`journal_entry`) VALUES ('$journalEntry')";
+    $query = mysqli_query($conn, $sql);
+    if ($query) {
+        echo 'Entry Successful! It worked Professor Foultz!';
+    } else {
+        echo 'Error Ocurred, oh god dont look professor';
     }
+}
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  include_once 'connect.php';
+include_once 'connect.php';
 ?>
 
 <!DOCTYPE html>
@@ -13,16 +13,38 @@
 
 <body>
   <h1> Daily Journal!</h1>
-  <h2> What are your intentions today?</h2>
-  <form action='addJournal.php' method="POST">
-    <label for="user"></label>
-    <textarea type='text' name='journalEntry' id="journalEntry" rows="5" cols="50" required></textarea>
-    <br> <br>
-    <input type='submit' name='submit' id="submit" />
-  </form>
+  <h2 id="current_date">
+    </p>
+    <script>
+      date = new Date();
+      year = date.getFullYear();
+      month = date.getMonth() + 1;
+      day = date.getDate();
+      document.getElementById("current_date").innerHTML = month + "/" + day + "/" + year;
+    </script>
+    <h2> What are your intentions today?</h2>
+    <form action='addJournal.php' method="POST">
+      <label for="user"></label>
+      <textarea type='text' name='journalEntry' id="journalEntry" rows="5" cols="50" required></textarea> <p></p>
+      <small>There is a 250 wordcount limit!</small>
+      <br> <br>
+      <input type='submit' name='submit' id="submit" />
+    </form>
 
-  <h4>PreviousEntries:</h4>
-  <br>
+    <h4>PreviousEntries:
+      <hr>
+    </h4>
+    <?php
+    $sql = "SELECT * FROM journals;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    if ($resultCheck > 0) {
+      while ($row = mysqli_fetch_assoc($result)) {
+        echo $row['journal_entry'] . "<hr><br>";
+      }
+    }
+    ?>
+    <br>
 </body>
 
 </html>
