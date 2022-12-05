9e4fe74892b9b190f3aae13fe0c4caa52f89f3c2
hlmathis14 <107157111+hlmathis14@users.noreply.github.com>
2022-12-04 23:25:53 -0500

    final changes
    completed

--- add.php
+++ add.php
@@ -98,7 +98,7 @@ do {
               <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
               </div>
             </div>
-        </div>      
+        </div>
         ";
     }
     ?>
